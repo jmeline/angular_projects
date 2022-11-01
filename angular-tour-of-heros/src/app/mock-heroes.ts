55e1e22f2c6afd881e6beb547e2de63fbf33a276
datsukai <melissa.meline@gmail.com>
2022-10-31 20:29:29 -0700

    Update mock-heroes.ts

--- angular-tour-of-heros/src/app/mock-heroes.ts
+++ angular-tour-of-heros/src/app/mock-heroes.ts
@@ -1,6 +1,7 @@
 import { Hero } from "./hero"
 
 export const HEROES: Hero[] = [
+  { id: 01, name: 'Juxebox Hero'},
   { id: 12, name: 'Dr. Nice'},
   { id: 13, name: 'Bombasto'},
   { id: 14, name: 'Celeritas'},
@@ -9,11 +10,5 @@ export const HEROES: Hero[] = [
   { id: 17, name: 'Dynama'},
   { id: 18, name: 'Dr. IQ'},
   { id: 19, name: 'Magma'},
-<<<<<<< Updated upstream
   { id: 20, name: 'Tornado'}
-=======
-  { id: 20, name: 'Tornado'},
-  { id: 21, name: 'Scorpion'},
-  { id: 22, name: 'Rename me'}
->>>>>>> Stashed changes
 ]
